39b43d1484b85713816df52cee807b68d5c9247a
jinzhe-mu <1421827450@qq.com>
2021-12-30 16:37:49 +0800

    while的使用

---
+++ 1_使用基础/03_while循环.py
@@ -0,0 +1,13 @@
+count = 0
+while count < 5:
+    print(count, "小于5")
+    count = count + 1
+else:
+    print(count, "大于或等于5")
+
+
+flag = 1
+while (flag) : print('欢迎来到xxx')
+print("Python测试开发")
+
+
