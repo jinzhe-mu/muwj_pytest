6d0a03b30e40dbb7014a249fd098c45f2e274bb3
jinzhe-mu <1421827450@qq.com>
2022-01-11 16:03:38 +0800

    引用系统模块

---
+++ 2_python模块/01_引用系统模块.py
@@ -0,0 +1,13 @@
+import sys
+import os
+import re
+import json
+import time
+# 打印s模块拥有的属性值，其他用法可参照官网进行使用
+print(sys.path)
+print(os.path)
+print(re.ASCII)
+print(json.JSONDecodeError)
+time.sleep(1)
+print(json.JSONDecodeError)
+
