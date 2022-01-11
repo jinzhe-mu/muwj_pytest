50b0c2ba63d9bd0cc2dab3f097a319fbc640eb22
jinzhe-mu <1421827450@qq.com>
2022-01-11 13:52:41 +0800

    字典的定义方式

---
+++ 1_使用基础/16_字典定义.py
@@ -0,0 +1,11 @@
+# 字典的定义
+dict_one = {}
+dict_two = dict(a=1, b=3)
+dict_three = {"a": 1, "b": 4}
+
+print("dict_one:", type(dict_one), dict_one)
+print("dict_two:", type(dict_two), dict_two)
+print("dict_three:", type(dict_three), dict_three)
+
+
+
