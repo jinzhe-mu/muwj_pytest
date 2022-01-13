3815e81331bd5db937f538a3e3d0cab6ca3aea15
jinzhe-mu <1421827450@qq.com>
2022-01-13 09:32:10 +0800

    字面量%格式化输出

---
+++ 3_python输入输出/01_字面量插值-格式化输出.py
@@ -0,0 +1,13 @@
+print("my name is muwj")
+
+# 字符串的引用 %s
+name = 'muwj'
+print("my name is %s" % name)
+# 十进制整数的引用 %d
+age = 3
+print("my name is %s, my age is %d" % (name, age))
+# 浮点数的引用 %f
+weight = 55.44
+print("my name is %s, my age is %d, my weigh is %f" % (name, age, weight))
+print("my name is %s, my age is %d, my weigh is %.2f" % (name, age, weight))
+
