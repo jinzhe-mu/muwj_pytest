1e2b2df59157cecf02316e94c9f8f3bba0f12bb9
jinzhe-mu <1421827450@qq.com>
2022-01-17 15:36:36 +0800

    异常的一般处理

---
+++ 4_python错误与异常/02_异常.py
@@ -0,0 +1,28 @@
+# 除零异常
+def div(a, b):
+    return a/b
+print(div(1, 0))
+
+
+
+# 名称异常
+num = 1
+if name >= 1:
+    print("num<1")
+elif num > 100:
+    print("num<1")
+
+# 索引异常
+list1 = [1, 2, 3]
+print(list1[3])
+
+# 键异常
+dic1 = {'name':'tom'}
+print(dic1['age'])
+
+
+# 值异常
+num1 = input("请输入一个值：")
+print(int(num1))
+
+
